2d7d45588f0e3eff28d9f0ba5b0a526a4867c8c9
ZJGP <1186388488@qq.com>
2019-10-26 09:24:56 +0800

    天猫数据爬取

--- Ecommerce/TianMao/TMIngress.py
+++ Ecommerce/TianMao/TMIngress.py
@@ -1,21 +1,26 @@
 # Author:Aliex ZJ
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding:utf-8 -*-
 import datetime
 import json
+import os
 import random
 import re
+import pandas as pd
 from time import sleep
 
 import requests
 from lxml import etree
+from pandas.errors import EmptyDataError
 
 from Utils.fileUtils import fileUtils
+from Utils.mysqlUtils import insertIntoMysql, truncateTable
 
 now_time = datetime.datetime.now().strftime('%Y-%m-%d')
 
-# 获取商品评论
-def TianMaoCommentCases(index,url_list):
+
+# 获取商品评论和明细
+def TianMaoCommentCases(index, url_list):
     # 保存评论数据集合
     rate_list = []
 
@@ -47,12 +52,16 @@ def TianMaoCommentCases(index,url_list):
 
     # 获取商品信息
     shop_response = requests.get(refer_url).text
+    # 商品价格
+    reserve_Price = re.findall('"reservePrice":"(\d*\.\d*)"', str(shop_response))[0]
+    # defaultitem_Price = re.findall('"defaultItemPrice":"(\d*\.\d*)"', str(shop_response))[0]
     selector = etree.HTML(shop_response)
     shop_items = selector.xpath('//*[@id="J_AttrUL"]/li')
+    # 详细信息
     shop_list = []
     for shop_item in shop_items:
-        shop_list.append(shop_item.xpath('./text()')[0].replace('\xa0',''))
-    print("产品信息："+str(shop_list))
+        shop_list.append(shop_item.xpath('./text()')[0].replace('\xa0', ''))
+    print("产品信息：" + str(shop_list))
     print("睡眠15s，开始获取评论信息")
     sleep(15)
 
@@ -64,20 +73,29 @@ def TianMaoCommentCases(index,url_list):
 
     # 2.获取总页数和需要分页的个数
     comment_count = json.loads(comment_datas)['rateDetail']['rateCount']['total']
-    comment_pages = int(int(comment_count)/20)+2
-    print("总评论数："+str(comment_count)+"===总页数："+str(comment_pages)+"===等待5s")
-    sleep(3)
+    comment_pages = int(int(comment_count) / 20) + 2
+    print("总评论数：" + str(comment_count) + "===总页数：" + str(comment_pages) + "===等待5s")
+    sleep(12)
+
+    # 获取平均评价评分
+    avgrate_url = rate_url.replace('rate.tmall.com','dsr-rate.tmall.com')\
+        .replace('list_detail_rate.htm','list_dsr_info.htm')\
+        .replace('&order=3&currentPage=1','')
+    avg_response = requests.get(avgrate_url, headers=headers1).text
+    avg_rate = json.loads(re.findall('{.*}', avg_response)[0])['dsr']['gradeAvg']
+    print("该商品平均评分:"+str(avg_rate)+"分,等待15s")
+    sleep(12)
 
     # 3.循环获取评论
-    for comment_page in range(1,comment_pages):
+    for comment_page in range(1, comment_pages):
         # 随机等待时间
-        sleep_list = [10,11,12,13,14,15,16,17,18,19,20]
+        sleep_list = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
         sleep_time = random.choice(sleep_list)
-        print('====睡眠'+str(sleep_time)+'s，总共'+str(comment_pages)+'页,开始爬取第'+str(comment_page)+'页评论====')
+        print('====睡眠' + str(sleep_time) + 's，总共' + str(comment_pages) + '页,开始爬取第' + str(comment_page) + '页评论====')
         sleep(sleep_time)
         # 分页请求网址
-        comment_url= rate_url.replace('currentPage=1','currentPage={0}').format(comment_page)
-        #获取随机cookie
+        comment_url = rate_url.replace('currentPage=1', 'currentPage={0}').format(comment_page)
+        # 获取随机cookie
         cookie_list = fileUtils().getCsvFile('../Data/Cookie.csv')
         cookie = random.choice(cookie_list)[0]
         # 获取随机浏览器信息
@@ -103,10 +121,12 @@ def TianMaoCommentCases(index,url_list):
                 rateContent = rate['rateContent']
                 auctionSku = rate['auctionSku']
                 cmsSource = rate['cmsSource']
-                if(rateContent !='此用户没有填写评论!'):
-                    rate_list.append({'rateDate':rateDate,'rateContent':rateContent,'auctionSku':auctionSku,
-                                      'cmsSource':cmsSource,'shop_list':str(shop_list),
-                                      'topic':topic,'store_name':store_name,'shop_title':shop_title,'add_date':now_time})
+                if (rateContent != '此用户没有填写评论!'):
+                    rate_list.append({'rate_date': rateDate, 'rate_content': rateContent, 'auction_sku': auctionSku,
+                                      'cms_source': cmsSource, 'shop_list': str(shop_list),'reserve_rice':reserve_Price,
+                                      'avg_rate':avg_rate,
+                                      'topic': topic, 'store_name': store_name, 'shop_title': shop_title,
+                                      'add_date': now_time})
                     print(rateContent)
         except Exception:
             print("Error: 没有找到网页内容")
@@ -116,22 +136,43 @@ def TianMaoCommentCases(index,url_list):
         print('===========================================')
     # print(rate_list)
     # 保存数据到文件
-    fileUtils().saveAsCsv(rate_list,'./Data/Rates/{0}'.format(str(int(index)+1)+shop_title))
+    fileUtils().saveAsCsv(rate_list, './Data/Rates/{0}'.format(str(int(index) + 1)))
+    sleep(10)
 
+# 保存到数据库
+def save_to_mysql(file_addr):
+    # 清空数据
+    truncateTable('spider','rates_tianmao')
+    for dirs in os.walk(file_addr):
+        fileList = dirs[2]
+        for fileName in fileList:
+            # 读取每个文件
+            csvFile=file_addr+'/{0}'.format(fileName)
+            print('开始存储'+str(csvFile)+'数据到mysql')
+            try:
+                resData = pd.read_csv(csvFile,encoding='utf-8')
+                # 去除空值
+                resData = resData.astype(object).where(pd.notnull(resData), None)
+                # 插入数据库
+                insertIntoMysql(resData,'spider','rates_tianmao')
+            except EmptyDataError:
+                print("当前商品无评论")
 
+            sleep(2)
 
 if __name__ == '__main__':
-
 
     # 获取商品url和评论的url
     url_lists = fileUtils().getCsvFile('./Data/store_url.csv')
 
     # 爬取商品评论
-    for index in range(0,1):
-        url_list = url_lists[index]
-        print(url_list)
-        # 获取天猫评论
-        TianMaoCommentCases(index,url_list)
+    # for index in range(143, 169):
+    #
+    #     url_list = url_lists[index]
+    #     print(url_list)
+    #     # 获取天猫评论
+    #     TianMaoCommentCases(index, url_list)
 
-    # 保存数据到文件
-    # print(rate_list)
+    file_addr = './Data/Rates'
+    save_to_mysql(file_addr)
+
--- Ecommerce/TianMao/Test/Test.py
+++ Ecommerce/TianMao/Test/Test.py
@@ -1,6 +1,7 @@
 # Author:Aliex ZJ
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
+import re
 import time
 
 import requests
@@ -13,19 +14,33 @@ headers = {
     "accept": "*/*",
     "accept-encoding": "gzip, deflate, br",
     "accept-language": "zh-CN,zh;q=0.9",
-    "cookie": "cna=L0CwFSqRDGgCAd9EwPN4s9v5; otherx=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0; x=__ll%3D-1%26_ato%3D0; t=3f886137b6f604509bea316d5773e8af; _tb_token_=566eee53ee01; cookie2=1b75d79b47265d251e10fab5baa12bd3; _m_h5_tk=449dee344f5265b55448239d53d1d5ea_1571223838598; _m_h5_tk_enc=8b2015d089a666045b850d29cb1dffea; dnk=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; uc1=lng=zh_CN&cookie14=UoTbnKFVSMp%2FgQ%3D%3D&existShop=false&pas=0&cookie16=UIHiLt3xCS3yM2h4eKHS9lpEOw%3D%3D&tag=8&cookie21=VT5L2FSpczFp&cookie15=URm48syIIVrSKA%3D%3D; uc3=vt3=F8dByuDsdUk70sMmjXw%3D&lg2=URm48syIIVrSKA%3D%3D&id2=UUpjN4zrNdJETg%3D%3D&nk2=05emRgpWdtZcceNuDGg%3D; tracknick=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; lid=%E7%99%BD%E7%99%BD%E7%9A%84%E7%8B%97%E5%B0%BE%E5%B7%B4%E8%8A%B1; _l_g_=Ug%3D%3D; uc4=id4=0%40U2gp9xkNePhwkA3mujHk7ASEEfB0&nk4=0%400SwjusWimre0H6WrAcFT6cSTuthSIm6c8g%3D%3D; unb=2221579274; lgc=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; cookie1=VWsu%2BpKF9hE9HePBZc%2FExBxRmsDaR97K6DS4nKh4iu0%3D; login=true; cookie17=UUpjN4zrNdJETg%3D%3D; _nk_=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; sg=%E8%8A%B145; csg=0fec1004; enc=MlWQUta4quK3OLr1eBmlnoMWCzPV9ufOqEdL33TbR7vzXA9n50GScF4XSpXHihsa6aMUMr27RjIKS9s782TA2Q%3D%3D; whl=-1%260%260%260; l=dBSH0T-4q4bTAJ9WBOCgIZMInnbOGLAVguWXRJ8ei_5dasTvxn7Okgzu8U96cjWcGwLB4nnADjetLe4TJy6foTB7K9cdvdHJBef..; isg=BE5OBQzbnx-F8CsAL8ODKHseiiQaWkiduVNJQniX1dEf2-w14F4D2WpdE0cSQwrh",
-    # "referer": "https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-18877415626.1339.10f56546p15MJZI&id=596624138584&sku_properties=8369595:5557472",
+    "cookie": "cna=L0CwFSqRDGgCAd9EwPN4s9v5; otherx=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0; x=__ll%3D-1%26_ato%3D0; t=3f886137b6f604509bea316d5773e8af; _tb_token_=566eee53ee01; cookie2=1b75d79b47265d251e10fab5baa12bd3; _m_h5_tk=449dee344f5265b55448239d53d1d5ea_1571223838598; _m_h5_tk_enc=8b2015d089a666045b850d29cb1dffea; dnk=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; lid=%E7%99%BD%E7%99%BD%E7%9A%84%E7%8B%97%E5%B0%BE%E5%B7%B4%E8%8A%B1; whl=-1%260%260%260; x5sec=7b22726174656d616e616765723b32223a226364373332303536336432373632316434303834333838643033356339393732434965306d2b3046454c62633135534431704c6464686f4d4d6a49794d5455334f5449334e447378227d; uc1=existShop=false&cookie21=VFC%2FuZ9ainBZ&pas=0&cookie15=W5iHLLyFOGW7aA%3D%3D&cookie16=Vq8l%2BKCLySLZMFWHxqs8fwqnEw%3D%3D&tag=8&cookie14=UoTbnKFVTCOKJg%3D%3D&lng=zh_CN; uc3=id2=UUpjN4zrNdJETg%3D%3D&vt3=F8dByuDsdUXD9npZbSQ%3D&lg2=WqG3DMC9VAQiUQ%3D%3D&nk2=05emRgpWdtZcceNuDGg%3D; tracknick=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; _l_g_=Ug%3D%3D; uc4=id4=0%40U2gp9xkNePhwkA3mujHk7ASAPgyW&nk4=0%400SwjusWimre0H6WrAcFT6cSTuthSJsZbtw%3D%3D; unb=2221579274; lgc=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; cookie1=VWsu%2BpKF9hE9HePBZc%2FExBxRmsDaR97K6DS4nKh4iu0%3D; login=true; cookie17=UUpjN4zrNdJETg%3D%3D; _nk_=%5Cu767D%5Cu767D%5Cu7684%5Cu72D7%5Cu5C3E%5Cu5DF4%5Cu82B1; sg=%E8%8A%B145; csg=cc99f275; enc=B2JID5w8jPfQ4Tmstb2yNtMyWzqqyQqbLWe7bGuqziBTY%2B%2Blk1oKU1jmJlpeeZZH9de7CSzOp0s8heJya%2F2xyw%3D%3D; l=dBSH0T-4q4bTAGh3BOCgIZMInnbttLAfguWXRJ8ei_5Ij_LEg97OkgzTtEp6cjWcGMYB4nnADjetYecu-yWfoTB7K9cdvdHJBef..; isg=BMXFPHNuNOLtJBAFsB6YxTxbwQH1FyNAXvbSh8cqyfwTXuTQj9PY5TL4aMINHpHM",
+    "referer": "https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-18877415626.1339.10f56546p15MJZI&id=596624138584&sku_properties=8369595:5557472",
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3732.400 QQBrowser/10.5.3819.400"}
 
+# url  = 'https://dsr-rate.tmall.com/list_dsr_info.htm?itemId=596624138584&spuId=1243962245&sellerId=2817130358' \
+#        # '&groupId&_ksTS=1571271589884_207&callback=jsonp208'
+# print(url)
+# url = 'https://rate.tmall.com/list_detail_rate.htm?itemId=596624138584&spuId=1243962245&sellerId=2817130358&order=3&currentPage=1'
+# url = url.replace('rate.tmall.com','dsr-rate.tmall.com') \
+#     .replace('list_detail_rate','list_dsr_info.htm') \
+#     .replace('&order=3&currentPage=1','')
+url = 'https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-18877415626.1339.10f56546p18MJZI&id=594730742591&sku_properties=8369595:5557472'
+print(url)
+response = requests.get(url, headers=headers)
+data = response.text
+print(data)
+comment_datas = re.findall('{.*}', data)[0]
+print(comment_datas)
+print(type(comment_datas))
+res = dict(comment_datas)['dsr']['gradeAvg']
+print(res)
 
-url = "https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-18877415626.1339.10f56546p15MJZI&id=596624138584"
-response = requests.get(url).text
+# url = "https://detail.tmall.com/item.htm?spm=a1z10.3-b-s.w4011-18877415626.1339.10f56546p15MJZI&id=596624138584"
+# response = requests.get(url).text
 # print(response)
-selector = etree.HTML(response)
-item = selector.xpath('//*[@id="J_AttrUL"]/li')
-for i in item:
-    print(i)
-    print(i.xpath('./text()'))
+# print(re.findall('"reservePrice":"(\d*\.\d*)"',str(response)))
+# print(re.findall('"defaultItemPrice":"(\d*\.\d*)"',str(response)))
 
 # option = webdriver.ChromeOptions()
 # # option.add_argument('headless')
