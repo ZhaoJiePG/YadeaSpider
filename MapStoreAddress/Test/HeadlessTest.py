61c5ab5ba709f19704f37c35d8a4cc7f0cbcb768
ZJGP <1186388488@qq.com>
2019-11-26 15:10:40 +0800

    买塑

--- MapStoreAddress/Test/HeadlessTest.py
+++ MapStoreAddress/Test/HeadlessTest.py
@@ -10,17 +10,47 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-
-# location='/server/python/selenium/geckodriver'
-location='D:\Maven\YadeaSpider\MapStoreAddress\geckodriver.exe'
-
-driver = webdriver.Firefox(executable_path=location)
+timeout=2
+option = webdriver.ChromeOptions()
 # option.add_argument('headless')
-url = 'https://map.baidu.com'
+# 要换成适应自己操作系统的chromedriver
+driver = webdriver.Chrome(
+    executable_path='../chromedriver.exe',
+    chrome_options=option)
+
+
+# option.add_argument('headless')
+url = 'http://weixin.sogou.com/weixin?type=1&s_from=input&query=买塑观察'
+
 
 # 打开网站
 driver.get(url)
 
+# 模拟点击“搜索”
+search_button = WebDriverWait(driver, timeout).until(
+    lambda d: d.find_element_by_xpath('//li[@id="sogou_vr_11002301_box_0"]/dl[3]/dd/a'))
+search_button.click()
+sleep(2)
+
+# 切换页面
+window_handles = driver.window_handles
+# 切换到右边的下一个页面
+driver.switch_to.window(window_handles[-1])
+
+# print(driver.page_source)
+xpath_date = etree.HTML(driver.page_source)
+name = xpath_date.xpath('//div[@id="js_content"]/table[14]/tbody/tr/td[1]/span/strong/text()')[0]
+area = xpath_date.xpath('//*[@id="js_content"]/table[14]/tbody/tr/td[2]/span/text()')[0]
+price = xpath_date.xpath('//*[@id="js_content"]/table[14]/tbody/tr/td[3]/span/text()')[0]
+name1= 'ABS塑件'
+print(name +area+price)
+
+xpath_date = etree.HTML(driver.page_source)
+name = xpath_date.xpath('//*[@id="js_content"]/table[23]/tbody/tr/td[1]/span/strong/text()')[0]
+area = xpath_date.xpath('//*[@id="js_content"]/table[23]/tbody/tr/td[2]/span/text()')[0]
+price = xpath_date.xpath('//*[@id="js_content"]/table[23]/tbody/tr/td[3]/span/text()')[0]
+
+print(name +area+price)
+
 # 打印当前页面标题
-print(driver.title)
-driver.close()
+# driver.close()
